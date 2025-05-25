27f11dbaa8864dbd1eb0f7e79cd30aa53c445168
Fefrancine <fernanda.francine.f@gmail.com>
2025-05-25 10:59:59 -0300

    fix: files update

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import index from '@/views/index.vue'   
+import bonecas from '@/views/bonecas.vue'
+import sacola from '@/views/sacola.vue'
+import saibaMais from '@/views/saiba-mais.vue'
+
+const routes = [
+    {
+        path: '/index',
+        name: 'Index',
+        component: index
+    },
+    {
+        path: '/bonecas',
+        name: 'Bonecas',
+        component: bonecas
+    },
+    {
+        path: '/sacola',
+        name: 'Sacola',
+        component: sacola
+    },
+    {
+        path: '/saiba-mais',
+        name: 'SaibaMais',
+        component: saibaMais
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
